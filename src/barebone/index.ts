c4bab52313ae616e6b82d04888c245814e81af5a
yuan tan <dyxtan@gmail.com>
2023-08-17 17:53:16 +1200

    add typing for store and action functions

--- src/barebone/index.ts
+++ src/barebone/index.ts
@@ -32,7 +32,7 @@ export interface StoreOptions<State = any, Name extends string = string, A exten
  * Functions for manipulating the state.
  */
 export interface Actions<State = any> {
-  [key: string]: ((state: State)=>State) | ((state: State, payload: any) => State);
+  [key: string]: ((state: State, payload?: any) => State);
 }
 
 /**
@@ -54,61 +54,74 @@ export type ExtractStoreName<Name extends string, State> = {
   [key in Name]: State;
 }
 
-export type StateListener = Map<unknown, (state: any)=>void>
+type SelectFn<S, T extends (state: S)=> ReturnType<T>> = (selectFn: T) => ReturnType<T>
 
+/** Keeps track of the states in components that are using the store. */
+export type StateListener = Map<unknown, (state: any)=>void>
+/** The state of the store */
 export type State<Name extends string = string, S = any> = {[key in Name]: S}; 
 
 export type CreateStoreResults<S, N extends string, A extends Actions<S>> = ExtractStoreName<N, S> & ActionsWithoutState<A>
 
-export const createStore = <S, Name extends string, A extends Actions<S>>(options: StoreOptions<S, Name, A>)=> {
+/**
+ * Creates a store that keeps track of a user defined state.
+ */
+export const createStore = <S, Name extends string, A extends Actions<S>>(options: StoreOptions<S, Name, A>):
+[SelectFn<State<Name, S>, (state: State<Name, S>) => S>, SelectFn<ActionsWithoutState<A>['actions'], (actions: ActionsWithoutState<A>['actions']) => ActionsWithoutState<A>['actions'][keyof ActionsWithoutState<A>['actions']]>] => {
   const state = {[options.name]: options.initialState} as  State<Name, S>;
   const stateListeners:StateListener = new Map();
-  /** Select actions */
-  const useActionSelect = createActions(options.actions, state, options.name, stateListeners);
 
-  /** Select State */
+  // Construct the hooks that are use to retrieve the
+  // state and actions.
+  const useActionSelect = createActions(options.actions, state, options.name, stateListeners);
   const useStoreSelect = createStoreHook(state, options.name, stateListeners);
 
-  return [useStoreSelect, useActionSelect] as [typeof useStoreSelect, typeof useActionSelect];
+  return [useStoreSelect, useActionSelect];
 };
 
 /**
- * Create a custom hook that returns the store state.
+ * Create a custom hook that can be used inside functions to
+ * retrieve the store state.
  */
-export const createStoreHook = <StoreState extends State, Name extends string>(state: StoreState, name: Name, stateListeners: StateListener)=>{
-  const useStoreSelect = <T extends (state: StoreState) => unknown>(select: T): ReturnType<T> =>{
+export const createStoreHook = <StoreState extends State, Name extends string>(state: StoreState, name: Name, stateListeners: StateListener) =>{
+  /**
+   * Creates a react state from the store and adds the setState
+   * function to the listener for triggering updates.
+   */
+  const useStoreSelect = <T extends (state: StoreState) => ReturnType<T>>(select: T): ReturnType<T> =>{
     const [storeState, setStoreState] = useState<StoreState>(state[name]);
     if(!stateListeners.has(setStoreState)){
       stateListeners.set(setStoreState, setStoreState);
     }
-    return select({[name]: storeState} as StoreState) as ReturnType<T>;
+    return select({[name]: storeState} as StoreState);
   } 
+
   return useStoreSelect;
 }
 
+/**
+ * Turns the user functions defined at store creation into actions
+ * use to interact with the store state.
+ */
 export const createActions = <StoreState extends State, A extends Actions, Name extends string>(actions: A, state: StoreState, storeName: Name, stateListeners: StateListener) =>{
   const result = {actions: {}} as ActionsWithoutState<A>;
+  // Construct a wrapper function for the action that hides
+  // the state. when this is called it uses the action to
+  // return a new state and then updates all the listeners
+  // with the new state.
   for(const key in actions){
     result.actions[key] = (payload?: unknown) => {
       const newState = actions[key](state[storeName], payload);
+      state[storeName] = newState;
       stateListeners.forEach(listener => {
         listener(newState);
       })
     }
   }
 
-  const useActionSelect = <T extends (state: ActionsWithoutState<A>['actions']) => unknown>(select: T): ReturnType<T> => {
-    return select(result.actions) as ReturnType<T>
+  const useActionSelect = <T extends (actions: ActionsWithoutState<A>['actions']) => ReturnType<T>>(select: T): ReturnType<T> => {
+    return select(result.actions)
   }
 
   return useActionSelect;
 };
-
-export const stuff = createStore({
-  name: 'dob', 
-  initialState: 4, 
-  actions: {
-    /** add stuff */
-    add: (state, to: number)=> state + to
-  }
-});
